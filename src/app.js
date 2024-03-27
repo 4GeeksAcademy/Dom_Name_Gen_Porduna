89160019bc33526d61b3d3ccb5acc6fd988c21b2
Pablo <143550869+arkngel212@users.noreply.github.com>
2024-03-27 20:21:50 +0000

    3 methods

--- src/app.js
+++ src/app.js
@@ -9,69 +9,61 @@ window.onload = function() {
   //write your code here
   console.log("Muerte a Prettier!!");
 
-  let pronoum = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
+  let pronoun = ["jodete", "odio"];
+  let adj = ["puto", "a"];
+  let noun = ["prettier", "prettier"];
 
-  let textfor = '<ul class="list-group">';
-  textfor +=
-    '<li class="list-group-item bg-success text-light">Using for length</li>';
-  for (let i = 0; i < ProcessingInstruction.length; i++) {
+  // Using for length
+
+  let textfor = '<ul class="list-group">'
+  textfor += '<li class="list-group-item bg-primary text-light">Using for length</li>'
+  for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        textFor +=
-          '<li class="list-group-item">' +
-          i +
-          "-" +
-          pronoun[i] +
-          j +
-          "-" +
-          adj[j] +
-          k +
-          "-" +
-          noun[k] +
-          "</li>";
+        textfor += '<li class="list-group-item bg-info">' + i + '-' + pronoun[i] + j + '-' + adj[j] + k + '-' + noun[k] + '</li>'
       }
     }
   }
-
-  textfor += "</ul>";
-  document.querySelector("#for").innerHTML = textfor;
+  textfor += '</ul>'
+  document.querySelector('#for').innerHTML = textfor;
 
   // Using for of
-  let textof = '<ul classs="list-group">';
-  textof +=
-    '<li class="list-group-item bg-success text-light">Using for of</li>';
-  for (constiterator of pronoun) {
+
+  let textof =  '<ul class="list-group">'
+  textof += '<li class="list-group-item bg-primary text-light">Using for of</li>'
+
+  for (const iterator of pronoun) {
     for (const item of adj) {
       for (const element of noun) {
         textof += `
-      <li class="list-group-item">
-      ${iterator}${item}${element}.com
-      </li>`;
+          <li class="list-group-item bg-info">
+            ${iterator}${item}${element}.com
+          </li>`
       }
     }
   }
-
-  textof += "</ul>";
-  document.querySelector("#of").innerHTML = textof;
+  textof += '</ul>'
+  document.querySelector('#of').innerHTML = textof;
 
   // Using method .map()
-  let textmap = '<ul class="list-group">';
-  textmap +=
-    '<li class="list-group-item bg-success text-light">Using textmap</li>';
 
-  pronoun.map(iterator => {
-    adj.map(item => {
-      noun.map(element => {
+  let textmap = '<ul class="list-group">'
+  textmap += '<li class="list-group-item bg-primary text-light">Using .map()</li>'
+
+  pronoun.map((iterator) => {
+    adj.map((item) => {
+      noun.map((element) => {
         textmap += `
-       <li class="list-group-item">
-        ${iterator}${item}${element}.com
-        </li> `;
-      });
-    });
-  });
+          <li class="list-group-item bg-info">
+            ${iterator}${item}${element}.com
+          </li>
+        `
+      })
+    })
+  })
 
-  textMap += "</ul>";
-  document.querySelector("#map").innerHTML = textMap;
+  textmap += '</ul>';
+  document.querySelector('#map').innerHTML = textmap;
+
+
 };
